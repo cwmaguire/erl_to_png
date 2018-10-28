e3be26bf39a0b400396257b5f85fdc2b31fe6267
Chris Maguire <cwmaguire@gmail.com>
2018-10-27 17:25:09 -0700

    Fix compile errors
    Remove the old code that depends on being in the same project as
    erl_png.

--- src/erl_to_png.erl
+++ src/erl_to_png.erl
@@ -1,31 +1,6 @@
--module(two_pixels).
-
--include("png.hrl").
+-module(erl_to_png).
 
 -export([render/0]).
 
 render() ->
-    Pixels = pixels(),
-    Header = #header{width = 100,
-                     height = 100,
-                     bit_depth = 8,
-                     color_type = 6,
-                     compression = 0,
-                     filter = 0,
-                     interlace = 0},
-    Png = #png{header = Header,
-               background = <<255,0,0>>,
-               physical = {100, 100, 0},
-               srgb = 0, % rendering intent
-               text = [],
-               data = <<>>,
-               pixels = Pixels,
-               other = []},
-    png:write(Png, "z.png").
-
-pixels() ->
-    Pixels = [#px{r = 255,
-                  g = 100,
-                  b = 100,
-                  a = 255} || _X <- lists:seq(101, 200)],
-    [Pixels || _ <- lists:seq(1,100)].
+    ok.
