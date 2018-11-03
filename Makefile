42d862946221e1c5012704a5c4712bdcffa414dd
Chris Maguire <cwmaguire@gmail.com>
2018-11-03 00:54:55 -0700

    WIP: roughing in plumbing
    Roughing in the plumbing for erl_to_tuples, erl_freetype and erl_png.
    
    1) Call erl_to_tuples:write_tuples/1 from erl_to_html to convert an
    Erlang source file into tuples of {Line, Text, Colour}.
    2) Render all the letters in the tuples as bitmaps of alpha values using
    erl_freetype
    3) Put the rendered letters together as pixel scanlines
    4) Send the scanlines to erl_png to be rendered.

--- Makefile
+++ Makefile
@@ -1,1 +1,11 @@
+PROJECT = erl_to_png
+PROJECT_DESCRIPTION = Render Erlang source to a PNG file
+PROJECT_VERSION = 0.0.1
+DEPS = erl_png erl_freetype
+
+dep_erl_png = git https://github.com/cwmaguire/erl_png master
+dep_erl_freetype = git https://github.com/cwmaguire/erl_freetype master
+
+ERLC_OPTS = -I deps/erl_png/include/
+
 include erlang.mk
--- src/erl_to_png.erl
+++ src/erl_to_png.erl
@@ -1,6 +1,118 @@
 -module(erl_to_png).
 
+-include("png.hrl").
+
 -export([render/1]).
 
-render(_) ->
-    ok.
+render(Filename) ->
+    Tuples = erl_to_tuples:write_tuples(Filename),
+    %{ok, Terms} = file:consult(Filename),
+
+    Letters = render_tuples(Tuples),
+    MaxHeight = lists:max([H || {_, _, H, _} <- Letters]),
+    MaxTop = lists:max([T || {_, _, _, T} <- Letters]),
+    Lines = lines(Letters),
+    Fun = fun(Line) ->
+                  lists:reverse(
+                    scanlines(Line,
+                              MaxHeight,
+                              MaxTop))
+          end,
+    Scanlines0 = lists:flatten([Fun(Line) || Line <- Lines]),
+    [Longest | _] = lists:sort(fun(L1, L2) -> length(L1) < length(L2) end,
+                               Scanlines0),
+    MaxLength = length(Longest),
+    Scanlines = [pad_length(SL, MaxLength) || SL <- Scanlines0],
+    Png = png(Scanlines),
+    erl_png:write_scanlines(Png, Filename ++ ".png").
+
+render_tuples(Tuples) ->
+    render_tuples([], Tuples).
+
+render_tuples(Letters, []) ->
+    lists:reverse(Letters);
+render_tuples(Letters, [{Line, <<Char:1/binary, Bin/binary>>, Colour} | Tuples]) ->
+    Letter = render_char:render_char([Char]),
+    render_tuples([Letter | Letters], [{Line, Bin, Colour} | Tuples]);
+render_tuples(Letters, [{Line, <<Char:1/binary>>, Colour} | Tuples]) ->
+    Letter = render_char:render_char([Char]),
+    render_tuples([{Line, Letter, Colour}| Letters], Tuples).
+
+lines(Letters) ->
+    lines(Letters, [[]]).
+
+lines([], Lines) ->
+    lists:reverse([lists:reverse(L) || L <- Lines]);
+lines([Letter | Letters], [[] | Lines]) ->
+    lines(Letters, [[Letter] | Lines]);
+lines([Letter = {Line, _, _} | Letters],
+      Lines = [[{DiffLine, _, _} | Letters] | Lines])
+    when Line /= DiffLine ->
+    lines(Letters, [[Letter] | Lines]);
+lines([Letter | Letters], [Line | Lines]) ->
+    lines(Letters, [[Letter | Line] | Lines]).
+
+scanlines(Line, Height, Top) ->
+    Scanlines = [<<>> || _ <- lists:seq(1, Height)],
+    scanlines(Line, Height, Top, Scanlines).
+
+scanlines([], _, _, Scanlines) ->
+    Scanlines;
+scanlines([Letter | Line], MaxH, MaxT, Scanlines0) ->
+    Scanlines = scanlines(Letter, MaxH, MaxT, Scanlines0, 1),
+    scanlines(Line, MaxH, MaxT, Scanlines).
+
+% We have MaxHeight (i.e. all) scanlines
+scanlines(_, MaxH, _, Scanlines, LineNo) when MaxH == LineNo ->
+    Scanlines;
+% We have all the scanlines
+scanlines({_, _, H, _}, MaxH, _, Scanlines, _)
+    when H == MaxH ->
+    Scanlines;
+% This top is lower than the max top, so we'll pad the top with a blank
+% line
+scanlines(Letter = {_, W, _, T}, MaxH, MaxT, Scanlines, LineNo)
+    when T < MaxT ->
+    BlankLine = list_to_binary([0 || _ <- lists:seq(1, W)]),
+    scanlines(Letter, MaxH, MaxT, [BlankLine | Scanlines], LineNo + 1);
+% The area below the baseline needs to be padded
+scanlines(Letter = {_, W, H, T}, MaxH, MaxT, Scanlines, LineNo)
+    when (MaxT - T) + H < LineNo ->
+    BlankLine = list_to_binary([0 || _ <- lists:seq(1, W)]),
+    scanlines(Letter, MaxH, MaxT, [BlankLine | Scanlines], LineNo + 1);
+% We're between Top and (Height - Top), which means there's data
+scanlines(Letter = {Bin, W, _, T}, MaxH, MaxT, Scanlines, LineNo) ->
+    TopPadding = MaxT - T,
+    Line = LineNo - TopPadding,
+    case Line of
+        0 ->
+            <<Scanline:W/binary, _/binary>> = Bin;
+        X ->
+            Skip = X * W,
+            <<_:Skip/binary, Scanline:W/binary, _/binary>> = Bin
+    end,
+    scanlines(Letter, MaxH, MaxT, [Scanline | Scanlines], LineNo + 1).
+
+pad_length(Scanline, MaxLine) when size(Scanline) < MaxLine ->
+    PaddingLength = MaxLine - size(Scanline),
+    Padding = list_to_binary([0 || _ <- lists:seq(1, PaddingLength)]),
+    <<Scanline/binary, Padding/binary>>;
+pad_length(Scanline, MaxLine) ->
+    <<Scanline:MaxLine/binary>>.
+
+png(Scanlines) ->
+    Header = #header{width = 400,
+                     height = 40,
+                     bit_depth = 8,
+                     color_type = 6,
+                     compression = 0,
+                     filter = 0,
+                     interlace = 0},
+    _Png = #png{header = Header,
+                background = <<255,0,0>>,
+                physical = {400, 40, 0},
+                srgb = 0, % rendering intent
+                text = [],
+                data = <<>>,
+                pixels = Scanlines,
+                other = []}.
